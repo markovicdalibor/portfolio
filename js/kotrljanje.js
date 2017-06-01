2774f1a042980520dc6e3a0673add256744495e8
DaliborMarkovic <cccp_100@hotmail.com>
2017-06-01 19:39:19 +0200

    Update kotrljanje.js

--- js/kotrljanje.js
+++ js/kotrljanje.js
@@ -3,7 +3,7 @@ jQuery(document).ready(function ($) {
     if(this.hash !== ""){
       event.preventDefault();
       var hash = this.hash;
-      $('html,body').animate({scrollTop:$(hash).offset().top},800, function(){
+      $('html,body').animate({scrollTop:$(hash).offset().top},900, function(){
         window.location.hash = hash;
       });
     };
