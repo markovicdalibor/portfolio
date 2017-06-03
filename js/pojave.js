bbfb78e8e8467d4c24200b339fc363dd450024b7
DaliborMarkovic <cccp_100@hotmail.com>
2017-06-03 22:55:58 +0200

    Add files via upload

---
+++ js/pojave.js
@@ -0,0 +1,20 @@
+jQuery(document).ready(function ($) {
+
+// da se pojavi about
+var menuPo = $('#navmenu').offset().top
+var opa = $('.opa0');
+function show(e,h) {
+  $(window).scroll(function(){
+  	var scrollPo = $(window).scrollTop();
+  	if(scrollPo>=menuPo+h){
+  		e.removeClass('e');
+  		e.addClass('opa1');
+  	}
+  });
+}
+show(opa.eq(0),-200);
+// da se pojavi portfolio
+show(opa.eq(1),50);
+// da se pojavi contac
+show(opa.eq(2),400);
+});
